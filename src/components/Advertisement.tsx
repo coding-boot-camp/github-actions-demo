a5fb101b9e688489b5c193967cedea51a9e2f579
Ron Bailey <jisacef@macx-00301.local>
2024-12-23 13:40:10 -0600

    Creating a PR for testing Part III

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -8,9 +8,9 @@ function Advertisement(props: AdvertisementProps) {
   return (
     <div className="advertisement">
       <p>{props.companyName}</p>
-      <p>{props.productDescription}/p
+      <p>{props.productDescription}</p>
       <p>{props.price}</p>
-    </div>
+    {/* </div> */}
   );
 }
 
